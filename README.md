7fbf92b070d65f5f4f80de7d002b1720c3182799
Shao-I Kao (k23038313) <shao-i.kao@kcl.ac.uk>
2024-07-09 00:26:58 +0100

    update

---
+++ README.md
@@ -0,0 +1,8 @@
+# candlechart_visualisation
+Financial data visualisation using D3
+
+- Scrolling the mouse wheel can zoom in and out
+- Click the buttons above to view the stock market trends of different companies.
+- The Financial Maps and Run Bubbles pages in the right-side panel are not yet completed (the design details can be seen in the images)
+
+* Run this project in WebStorm
--- js/interface.js
+++ js/interface.js
@@ -3,16 +3,14 @@ document.addEventListener("DOMContentLoaded", function() {
 
     buttons.forEach(function(button, index) {
         button.addEventListener("click", function() {
-            // 移除所有按钮的活动状态
+
             buttons.forEach(function(btn) {
                 btn.classList.remove("active");
             });
 
-            // 将当前点击的按钮设置为活动状态
             this.classList.add("active");
         });
 
-        // 预先设置第一个按钮为活动状态
         if (index === 0) {
             button.classList.add("active");
         }
